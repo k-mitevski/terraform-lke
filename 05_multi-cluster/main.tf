516e65eb611cc1e10fa95ee55ec5f5bf070ab62c
Kristijan <51249239+k-mitevski@users.noreply.github.com>
2021-04-22 17:41:58 +0200

    rename sources

--- 05_multi-cluster/main.tf
+++ 05_multi-cluster/main.tf
@@ -1,17 +1,17 @@
 module "dev_cluster" {
-    source          = "./root"
+    source          = "./cluster_module"
     env_name        = "dev"
     cluster_name    = "learnk8s"
     instance_type   = "g6-standard-2"
 }
 module "staging_cluster" {
-    source          = "./root"
+    source          = "./cluster_module"
     env_name        = "staging"
     cluster_name    = "learnk8s"
     instance_type   = "g6-standard-2"
 }
 module "prod_cluster" {
-    source          = "./root"
+    source          = "./cluster_module"
     env_name        = "prod"
     cluster_name    = "learnk8s"
     instance_type   = "g6-dedicated-4"
